adc4c1cba84a441552ae4082ee4f55dd78bbdf9e
mozrin <mozrin@gmail.com>
2025-12-15 10:33:15 +0700

    feat: Phase 009 Complete

--- lib/entity.dart
+++ lib/entity.dart
@@ -1,13 +1,21 @@
+import 'zone.dart';
+
 class Entity {
   final String id;
+  final Zone zone;
 
-  Entity({required this.id});
+  Entity({required this.id, this.zone = Zone.safe});
 
   Map<String, dynamic> toJson() {
-    return {'id': id};
+    return {'id': id, 'zone': zone.toJson()};
   }
 
   factory Entity.fromJson(Map<String, dynamic> json) {
-    return Entity(id: json['id'] as String);
+    return Entity(
+      id: json['id'] as String,
+      zone: json['zone'] != null
+          ? Zone.fromJson(json['zone'] as String)
+          : Zone.safe,
+    );
   }
 }
