af2bf9d72e0437c294f84c4d64b58479b49a2e11
mozrin <mozrin@gmail.com>
2025-12-15 13:11:30 +0700

    feat: Phase 015 Complete

--- lib/entity.dart
+++ lib/entity.dart
@@ -3,18 +3,34 @@ import 'entity_type.dart';
 
 class Entity {
   final String id;
+  final double x;
+  final double y;
   final Zone zone;
   final EntityType type;
 
-  Entity({required this.id, this.zone = Zone.safe, this.type = EntityType.npc});
+  Entity({
+    required this.id,
+    required this.x,
+    required this.y,
+    this.zone = Zone.safe,
+    this.type = EntityType.npc,
+  });
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'zone': zone.toJson(), 'type': type.toJson()};
+    return {
+      'id': id,
+      'x': x,
+      'y': y,
+      'zone': zone.toJson(),
+      'type': type.toJson(),
+    };
   }
 
   factory Entity.fromJson(Map<String, dynamic> json) {
     return Entity(
       id: json['id'] as String,
+      x: (json['x'] as num).toDouble(),
+      y: (json['y'] as num).toDouble(),
       zone: json['zone'] != null
           ? Zone.fromJson(json['zone'] as String)
           : Zone.safe,
